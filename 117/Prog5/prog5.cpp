1a5325403a5c870e65fea456b074d434419a75c6
Sergio <sergioavila2720@mail.fresnostate.edu>
2015-10-16 10:59:22 -0700

    Starting on Program 5 of this class (garbage collection)

---
+++ 117/Prog5/prog5.cpp
@@ -0,0 +1,17 @@
+/*
+In this program we will be simulating a garbage collection program
+we will use linked lists to do the lazy garbage collection
+*/
+# include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main (){
+
+
+	return 0;
+}
