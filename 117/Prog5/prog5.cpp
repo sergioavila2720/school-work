f64c9cd4c3cfdf74bb8a0a5cc3a503fe35d7e9d7
Sergio <sergioavila2720@mail.fresnostate.edu>
2015-10-21 15:51:51 -0700

    Did a version of the insertion sort. I dont think it's correct though.

--- 117/Prog5/prog5.cpp
+++ 117/Prog5/prog5.cpp
@@ -12,38 +12,104 @@ struct list1{
 	int key, mark;
 	list1 *next;
 };
+// This is an addition to the insertion sort in order for it to work. 
+// This is just for creatin a new node.
+list1* createNode(int key);
 
-void Insertion_sort(int [],int );
+// This will work for when we start inserting our keys into our arrays. 
+list1* Insertion_sort(int *, int);
 
 
 
 int main (){
 
+	int option;
+	list1 *array;
+	array = new list1[10];
 
-int *array;
-array = new int[10];
+	list1 *h1,*h2,*free_list;
+	h1 = NULL;
+	h2 = NULL;
+	free_list = NULL;
 
-list1 *h1,*h2,*free_list;
-h1 = NULL;
-h2 = NULL;
-free_list = NULL;
+	int arr[2] = {1,2};
+	
+
+	do {
+
+		cout << "1). Insert a list" << endl;
+		cout << "2). Delete" << endl;
+		cout << "3). Garbage Collector" << endl;
+		cout << "4). Display Memory" << endl;
+		cout << "5). Quit" << endl;
+
+		cout << "What do you want to do? ";
+		cin>> option;
+		cout << endl;
+
+		if (option == 1){
+			cout << "We are going to INSERT the list here.\n" <<endl;
+			list1 *mylist = Insertion_sort(arr, 3);
+			cout << &mylist << endl;
+
+		}else if (option == 2){
+			cout << "We are going to DELETE here.\n" << endl;
+		
+		}else if (option == 3){
+			cout << "We are going to call the GARBAGE COLLECTOR.\n" << endl;
+
+		}else if (option == 4){
+			cout << "We are going to DISPLAY THE MEMORY.\n" << endl;
+		}
+		else if (option == 5)
+			exit(1);
+		else
+			cout << "Sorry this is not an option try again.\n" << endl;
+
+
+	}while (option != 5);
+
+
+
+
 
 	return 0;
 
 
 }
 
-void Insertion_sort(int a[],int n){
+list1* createNode(int key){
 
-	 int key,i,j;
-	 for(j=2;j<=n;j++){
-	 	key=a[j];
-	 	i=j-1;
-	 	while((i>0)&&(a[i]>key)){
-	 		a[i+1]=a[i];
-	 		i=i-1;
-	 	}
+	list1 * n = new list1;
+	n -> key = key;
+	n ->next = 0;
 
-	 	a[i+1]=key;
+	return n;
+}
+
+
+list1* Insertion_sort(int *arr, int size){
+
+	list1* head;
+	head = createNode(arr[0]);
+	for (int i = 0; i < size; ++i){
+		list1 *prev = 0;
+		list1 *iter = head;
+
+		while(iter != 0){
+			if (iter ->key){
+				prev = iter;
+				iter = iter ->next;
+			}
+			else
+				break;
+		}
+
+		list1 *newNode = createNode(arr[i]);
+		if (prev == 0)
+			head = newNode;
 	}
+	return head;
+
+
 }
