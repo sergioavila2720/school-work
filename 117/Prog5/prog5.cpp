0e630c4a5f2d28bf3f311a805b07d28363baa2a2
Sergio <sergioavila2720@mail.fresnostate.edu>
2015-10-25 23:32:55 -0700

    I'm not finished with this lab. I'm in the middle of implementing the garbage collector.

--- 117/Prog5/prog5.cpp
+++ 117/Prog5/prog5.cpp
@@ -1,40 +1,43 @@
 /*
 In this program we will be simulating a garbage collection program
 we will use linked lists to do the lazy garbage collection
+I am running this program through the terminal in ubuntu.
+using g++ prog5.cpp -std=c++11
+
 */
 # include <iostream>
-
+# include <list>
+# include <stdlib.h>
+# include <stdio.h>
 
 using namespace std;
 
 struct list1{
 
-	int key, mark;
+	int key, mark, next1;
 	list1 *next;
+	list1 *listInfo;
 };
-// This is an addition to the insertion sort in order for it to work. 
-// This is just for creatin a new node.
-list1* createNode(int key);
+// Setting my two linked lists with it's head pointers at NULL
+	list1 *h1 = NULL;
+	list1 *h2 = NULL;
+	list1 *free_list;
+	list1 *array1 = new list1[10];
 
-// This will work for when we start inserting our keys into our arrays. 
-list1* Insertion_sort(int *, int);
-
+// Function prototypes.
+list1* createNode(int);
+void insertionSort (list1 **, list1*);
+void inserting();
+void Delete(list1*, int);
+void GarbageCollector(list1*, list1*, list1*);
+void Display();
 
 
 int main (){
 
-	int option;
-	list1 *array;
-	array = new list1[10];
+	int option, deleteuser;
 
-	list1 *h1,*h2,*free_list;
-	h1 = NULL;
-	h2 = NULL;
-	free_list = NULL;
-
-	int arr[2] = {1,2};
-	
-
+/*Implemented a do while loop to build the menu driven program */
 	do {
 
 		cout << "1). Insert a list" << endl;
@@ -49,11 +52,15 @@ int main (){
 
 		if (option == 1){
 			cout << "We are going to INSERT the list here.\n" <<endl;
-			list1 *mylist = Insertion_sort(arr, 3);
-			cout << &mylist << endl;
+			inserting();
 
 		}else if (option == 2){
 			cout << "We are going to DELETE here.\n" << endl;
+			cout << "What key do you want to delete? ";
+			cin >> deleteuser;
+			Delete(h1, deleteuser);
+			Display();
+
 		
 		}else if (option == 3){
 			cout << "We are going to call the GARBAGE COLLECTOR.\n" << endl;
@@ -78,38 +85,108 @@ int main (){
 
 }
 
-list1* createNode(int key){
+/* This is an addition to the insertion sort in order for it to work. 
+   This is just for creatin a new node.*/
+list1* createNode(int currKey){
 
 	list1 * n = new list1;
-	n -> key = key;
-	n ->next = 0;
+	n -> key = currKey;
+	n ->next = NULL;
 
 	return n;
 }
 
-
-list1* Insertion_sort(int *arr, int size){
-
-	list1* head;
-	head = createNode(arr[0]);
-	for (int i = 0; i < size; ++i){
-		list1 *prev = 0;
-		list1 *iter = head;
-
-		while(iter != 0){
-			if (iter ->key){
-				prev = iter;
-				iter = iter ->next;
-			}
-			else
-				break;
-		}
-
-		list1 *newNode = createNode(arr[i]);
-		if (prev == 0)
-			head = newNode;
+// This will work for when we start inserting our keys into our arrays. 
+void insertionSort (list1 **head, list1* new_node){
+	list1 *current;
+	//case for the head end.
+	if (*head == NULL || (*head) -> key >= new_node -> key){
+		new_node -> next = *head;
+		*head = new_node;
 	}
-	return head;
-
-
+	else {
+		//look for the node before insertion
+		current = *head;
+		while(current ->next != NULL && current -> next -> key < new_node -> key){
+			current = current -> next;
+		}
+		new_node -> next = current -> next;
+		current -> next = new_node;
+	}
 }
+/* instead of doing all these comands in the main function 
+I made a helper function to insert the keys*/
+void inserting(){
+
+	list1 *new_node = createNode(3);
+	insertionSort(&h1, new_node);
+
+	new_node = createNode(1);			
+	insertionSort(&h1, new_node);
+
+	new_node = createNode(4);
+	insertionSort(&h2, new_node);
+
+	new_node = createNode(5);
+	insertionSort(&h1, new_node);
+
+	new_node = createNode(2);
+	insertionSort(&h2, new_node);
+
+	new_node = createNode(9);
+	insertionSort(&h2, new_node);
+
+	new_node = createNode(8);
+	insertionSort(&h2, new_node);
+
+	new_node = createNode(4);
+	insertionSort(&h1, new_node);
+}
+/* In this function we can select and delete a specific node */
+void Delete(list1* head , int n){
+
+	list1 *p = head;
+
+	for (int i = 0; i < n-1 && p != NULL; i++){
+		p = p ->next;
+	}
+
+	if (p != NULL){
+		list1* del = p -> next;
+		p -> next = p -> next -> next;
+		delete del;
+	}
+	else 
+		delete p;
+}
+/* This is the garbage collection. In here i will implement the mark and sweep function. */ 
+void GarbageCollector(list1* head1, list1* head2, list1* freeL){
+this will iterate 
+	// through the nodes 
+	list1* iter;
+
+	/* in this next part is the mark function
+	this will search for h1, h2, and the freelist, 
+	and it will mark them with 1 at the begining */
+	for (iter = head1; iter != NULL; iter = iter -> next){
+		iter -> listInfo -> mark = 1;
+	}
+
+	for (iter = head2; iter != NULL; iter = iter -> next){
+		iter -> listInfo -> mark = 1;
+	}
+
+	for (iter = freeL; iter != NULL; iter = iter -> next){
+		iter -> listInfo -> mark = 1;
+	}
+
+	/* I could not finish implementing the garbage collector */
+}
+
+
+void Display(){
+	cout << "h1 = " << h1 -> listInfo -> key +1 << endl;
+}
+
+/* Due to a csci 144 midterm that i have on monday I could not finih
+this lab. I did as much as I could :( */
