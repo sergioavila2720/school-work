d6806f1ed28b7303c381bf5d0b69b97bc176a576
Sergio <sergioavila2720@mail.fresnostate.edu>
2015-11-04 15:57:24 -0800

    Keep working

--- 117/Prog6/prog6.cpp
+++ 117/Prog6/prog6.cpp
@@ -3,6 +3,47 @@
 
 using namespace std;
 
+class cell{
+	int info;
+	cell *next;
+	cell(int i) {info = i; next = this;}
+	cell(int i, cell *n) {info = i; next = n;}
+	friend class list;
+};
+
+class list{
+	cell *rear;
+	public:
+		list() {rear = new cell(0);}
+		~list() {while (!empty()) get();}
+		int empty() {
+			return rear == rear->next;
+		}
+	protected:
+		void add(int);
+		void push(int);
+		int get();
+};
+
+void list::push(int x){
+	rear -> next = new cell(x, rear -> next);
+}
+void list::add(int x){
+	rear -> info = x;
+	rear = rear -> next = new cell(0, rear -> next);
+}
+
+int list::get(){
+	if(empty() )
+		return 0;
+	cell *front = rear ->next;
+	rear -> next = front -> next;
+	int x = front -> info;
+	delete front;
+	return x;
+}
+
+
 int main(){
 
 
