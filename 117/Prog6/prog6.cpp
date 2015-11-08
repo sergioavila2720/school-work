8a8b189a483f7915cf861597aadc60e156595642
Sergio <sergioavila2720@mail.fresnostate.edu>
2015-11-07 21:14:15 -0800

    It works propertly now. I just need documentation, and I'm done.

--- 117/Prog6/prog6.cpp
+++ 117/Prog6/prog6.cpp
@@ -11,10 +11,6 @@ class cell{
 	friend class list;
 	friend class NC_list;
 };
-struct node{
-	int key;
-	node *next1;
-};
 /*
 
 class list{
@@ -29,20 +25,6 @@ class list{
 		void add(int);
 		void push(int);
 		int get();
-};
-class NC_list{
-	cell *rear;
-	cell *front;
-public:
-	NC_list(){front = new cell(0);}
-	~NC_list(){while (!empty()) get();}
-	int empty(){
-		return front == rear ->next;
-	}
-protected:
-	void add(int);
-	void push(int);
-	int get();
 };
 
 void NC_list::push(int x){
@@ -60,18 +42,7 @@ void list::add(int x){
 	rear -> info = x;
 	rear = rear -> next = new cell(0, rear -> next);
 }
-
-int NC_list::get(){
-	if(empty() )
-		return 0;
-	cell *front = rear ->next;
-	rear -> next = front -> next;
-	int x = front -> info;
-	delete front;
-	return x;
-}
-
-class queue: public NC_list {
+class queue: public list {
 public:
 	queue(){}
 	int Qget() {return NC_list::get();}
@@ -79,7 +50,7 @@ public:
 
 };
 
-class stack: private NC_list{
+class stack: private list{
 public:
 	stack(){}
 	int Spop() {return NC_list::get();}
@@ -87,131 +58,113 @@ public:
 	using NC_list::empty;
 };
 */
+
+struct node{
+	int key;
+	node *next1;
+};
+
 class NC_list {
-	node *rear, *front;
+	node *rear, *front, *p = NULL, *np = NULL;
 
 public:
 	NC_list(): rear(NULL), front(NULL){} 
 	void push(int);
 	void add(int);
 	int get();
-	void print();
 	int empty() {
 		return rear == rear->next1;
 	}
 };
 
-void NC_list::push(int x){
-	//rear -> next = new cell(x, rear -> next);
+void NC_list::add(int x){
 
-	node *n = new node();
-	n -> key = x;
+	np = new node;
+	np -> key = x;
+	np -> next1 = NULL;
 
-	if (rear)
-		n -> next1 = rear;
-	else{
-		n -> next1 = NULL;
-		front = n;
+	if(front == NULL){
+		front = rear = np;
+		rear -> next1 = NULL;
 	}
 
-	front = n;
+	else {
+		rear -> next1 = np;
+		rear = np;
+		rear -> next1 = NULL;
+	}
+}
+
+void NC_list::push(int x){	
+
+	np = new node;
+	np -> key = x;
+	np -> next1 = front;
+	front = np;
+
+
+
+/*
+	np = new node;
+	np -> key = x;
+
+	if (rear)
+		np -> next1 = rear;
+	else{
+		np -> next1 = NULL;
+		front = np;
+	}
+
+	front = np;
+	*/
 
 }
 int NC_list::get(){
 
-		node *temp = front;
-		int r = temp -> key;
+		p = front;
+		int r = p -> key;
 		front = front -> next1;
-		delete temp;
+		delete p;
 		return r;
-	}
-	/*
-	if(empty() )
-		return 0;
-	node *current = front;
-	front = front -> next1;
-	int x = current -> key;
-	delete front;
-	return x;
-	*/
-
-
-void NC_list::print(){
-
-	rear = front ->next1;
-
-
-	node *temp = NULL;
-	temp = front;
-	while(temp != NULL){
-		cout << temp -> key << endl;
-		temp = temp -> next1;
-
-		
-	}
 }
 
+class queue: public NC_list{
+public:
+	queue(){}
+	void Qput(int x){NC_list::add(x);}
+	int Qget() {return NC_list::get();}
+
+};
 
 class stack: private NC_list{
 public:
 	stack(){}
+	int Spop(){return NC_list::get();}
 	void Spush(int x){NC_list::push(x);}
-	int Spop() {return NC_list::get();}
-
-
-
 };
 
 int main(){
-/*
+
 	queue q1;
 	stack s1;
-
+	q1.Qput(1);
+	q1.Qput(2);
 	q1.Qput(3);
-	q1.Qput(5);
-	q1.Qput(7);
-
+	q1.Qput(4);	
+	
 	cout << q1.Qget() << endl;
 	cout << q1.Qget() << endl;
 	cout << q1.Qget() << endl;
 	cout << q1.Qget() << endl;
 
 	cout << endl;
-	
 
-	s1.Spush(2);
-	s1.Spush(4);
-	s1.Spush(6);
-
-	cout << s1.Spop() << endl;
-	cout << s1.Spop() << endl;
-	cout << s1.Spop() << endl;
-	cout << s1.Spop() << endl;
-
-	//cout << q1.empty();
-	//cout << s1.empty();
-
-	//s1.list();
-	*/
-	/*
-
-	stack s1;
-	s1.Spush(1);
-	s1.Spop();
-	s1.Spush(2);
-	s1.Spop();
-	s1.Spush(3);
-	s1.Spop();
-	s1.Spush(4);
-	s1.Spop();
-*/
-	stack s1;
-	s1.Spush(1);
-	s1.Spush(2);
-	s1.Spush(3);
 	s1.Spush(5);
-	
-	
+	s1.Spush(6);
+	s1.Spush(7);
+	s1.Spush(8);
+
+	cout << s1.Spop() << endl;
 	cout << s1.Spop() << endl;
 	cout << s1.Spop() << endl;
 	cout << s1.Spop() << endl;
