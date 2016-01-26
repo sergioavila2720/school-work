fa2cc16989e66fccec79c66f456ab2136eca90f6
Sergio <sergioavila2720@mail.fresnostate.edu>
2016-01-25 20:11:56 -0800

    Exercise #3 DONE got user input (name) and printed hello (name)

--- 130/Exercises_1-3/main.go
+++ 130/Exercises_1-3/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	myName := "Sergio Avila"
-    fmt.Printf("Hello, my name is " + myName + "\n")
+	var myName string
+   // fmt.Printf("Hello, my name is " + myName + "\n")
+	fmt.Print("Enter your name\n")
+	fmt.Scanf("%s", &myName) // i read that i can also use %v for the default format
+	fmt.Print("Hello ", myName, "\n")
 }
