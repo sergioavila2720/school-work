2bed77827423a79d2e3f617c93ccfa988c012721
Sergio <sergioavila2720@mail.fresnostate.edu>
2016-01-25 20:27:15 -0800

    var type

---
+++ 130/data_type/type.go
@@ -0,0 +1,18 @@
+
+package main
+
+import "fmt"
+
+ func main() {
+
+ 	integer := 123
+ 	float_num := 12.34
+ 	boolean := true 
+ 	strings := "hey"
+
+ 	fmt.Printf("%T\n", integer)
+ 	fmt.Printf("%T\n", float_num)
+ 	fmt.Printf("%T\n", boolean)
+ 	fmt.Printf("%T\n", strings)
+	
+}
