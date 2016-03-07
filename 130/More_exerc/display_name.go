940ae6d2b37bc14c7aab6c057c36c6194e297e19
Sergio <sergioavila2720@mail.fresnostate.edu>
2016-03-07 12:42:54 -0800

    display name

--- 130/More_exerc/display_name.go
+++ 130/More_exerc/display_name.go
@@ -11,22 +11,3 @@ func main() {
 	})
 	http.ListenAndServe(":8020", nil)
 }
-/*
-package main
-
-import (
-	"fmt"
-	"net/http"
-)
-
-func Url_Path(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "localhost:8080/name%v", req.URL.Path)
-}
-
-
-func main() {
-	http.HandleFunc("/", Url_Path)
-	http.ListenAndServe(":8040", nil)
-}
-
-*/
