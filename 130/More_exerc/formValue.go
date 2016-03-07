509359cdd4e1692f19b09b18bedda3a20a575e02
Sergio <sergioavila2720@mail.fresnostate.edu>
2016-03-07 13:18:55 -0800

    form value

--- 130/More_exerc/formValue.go
+++ 130/More_exerc/formValue.go
@@ -15,24 +15,3 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 
 }
-
-
-/*
-
-package main
-
-import (
-	"fmt"
-	"net/http"
-)
-
-func Url_Path(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "SomeName%v", req.FormValue("n"))
-}
-
-
-func main() {
-	http.HandleFunc("/", Url_Path)
-	http.ListenAndServe(":8080", nil)
-}
-*/
