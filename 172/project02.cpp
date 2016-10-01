7e4d1f9e5f1b382777500fe773968ea1395ffb64
Sergio <sergioavila2720@mail.fresnostate.edu>
2016-10-01 16:07:31 -0700

    Framework for program 2

---
+++ 172/project02.cpp
@@ -0,0 +1,156 @@
+/*
+Sergio Avila 
+ID: 108975197
+Csci: 172
+
+Compile: g++ project02.cpp -o run2 -lGL -lGLU -lglut
+run:     ./run2
+*/
+
+
+
+#include <string.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+bool WireFrame= false;
+
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+
+/* GLUT callback Handlers */
+
+static void resize(int width, int height)
+{
+     double Ratio;
+
+   if(width<=height)
+     {
+         glViewport(0,(GLsizei) (height-width)/2,(GLsizei) width,(GLsizei) width);
+         Ratio =  height/width;
+      }
+    else
+      {
+        glViewport((GLsizei) (width-height)/2 ,0 ,(GLsizei) height,(GLsizei) height);
+        Ratio = width /height;
+      }
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+	gluPerspective (50.0f,Ratio,0.1f, 100.0f);
+ }
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    gluLookAt(0,5,10,0.0,0.0,0.0,0.0,1.0,0.0);
+
+    if(WireFrame)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);		//Draw Our Mesh In Wireframe Mesh
+	else
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);		//Toggle WIRE FRAME
+
+    // your code here
+
+    glutSwapBuffers();
+}
+
+static void key(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 27 :
+        case 'q':
+            exit(0);
+            break;
+    }
+}
+
+void Specialkeys(int key, int x, int y)
+{
+    switch(key)
+    {
+    case GLUT_KEY_UP:
+    break;
+   }
+  glutPostRedisplay();
+}
+
+static void idle(void)
+{
+    glutPostRedisplay();
+}
+
+static void init(void)
+{
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+
+    glEnable(GL_DEPTH_TEST);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glShadeModel(GL_SMOOTH);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_LIGHTING);
+}
+
+
+/* Program entry point */
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutCreateWindow("GLUT Shapes");
+    init();
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(key);
+    glutSpecialFunc(Specialkeys);
+
+    glutIdleFunc(idle);
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
